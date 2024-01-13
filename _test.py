dd5f2b2426515b847ae212bf9cffed0ca68ef84e
GroveDG <grovedegroof@gmail.com>
2024-01-13 13:23:06 -0500

    Init
    The first batch of obvious emoji sounds

---
+++ _test.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from emoji import emojize, is_emoji, version
+from unicodedata import lookup
+
+def main():
+	files = Path().glob("*.wav")
+	emoji_characters = []
+	unicode_characters = []
+	falied_characters = []
+	for filepath in files:
+		emoji = emojize(f":{filepath.stem}:", language='alias')
+		if is_emoji(emoji):
+			emoji_characters.append(emoji)
+		else:
+			character = filepath.stem
+			try:
+				character = lookup(character)
+				unicode_characters.append(character)
+			except:
+				falied_characters.append(character)
+	print(emoji_characters)
+	print(unicode_characters)
+	print(falied_characters)
+
+if __name__ == "__main__":
+	main()
